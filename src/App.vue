d35a67890e0662d671a6ef9cfcad5db59f0ddb31
luzhixing12345 <luzhixing12345@163.com>
2022-11-06 02:35:17 +0800

    [feat] : finish init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <Service></Service>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Service from './components/Service'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Service
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scope>
+body,html,.main{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+body {
+  background-image: url("./assets/backgroundImage.jpg");
+  background-position: center;
+  background-size: cover; 
+  background-attachment: scroll;
 }
 </style>
---
+++ src/components/Service.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="list">
+        <div v-for="(service, index) in Services" :key="index" class="service-item">
+            <div>{{ service.name }}</div>
+            <div>{{ service.description }}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Service",
+    data() {
+        return {
+            Services: [
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+                { 'name': 'Attack Lab 画图', 'description': 'CS:APP第三次实验attack lab内存单元辅助画图' },
+            ]
+        }
+    },
+}
+</script>
+<style scoped>
+.list {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    overflow-y: auto;
+    /* justify-content: space-between; */
+}
+
+.list .service-item {
+    /* flex: 0 1 40%; */
+    margin-top: 100px;
+    border-radius: 15px;
+    width: 40%;
+    min-height: 15%;
+    background-color: #8eecf5;
+    opacity: 0.6;
+    margin: 50px auto;
+    box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
+    transition: all 1s;
+}
+
+.list .service-item:hover {
+    transition: all 1s;
+    box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+    opacity: 1;
+}
+
+.list .service-item :first-child {
+    font-size: 35px;
+    font-weight: bolder;
+    margin: 10px 10px;
+}
+
+.list .service-item :last-child {
+    font-size: 24px;
+    margin-left: 10px;
+}
+
+</style>
