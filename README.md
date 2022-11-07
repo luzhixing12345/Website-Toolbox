983bf04f096311ac01f00c6b41bf0d7533624b93
luzhixing12345 <luzhixing12345@163.com>
2022-11-07 16:57:46 +0800

    [docs] : add docker envs init setup script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # cloud-server
 
 个人云服务器配置及网站页面
+
+docker容器基本环节初始化脚本
+
+```bash
+wget https://github.com/luzhixing12345/cloud-server/blob/master/dockerInit.sh
+```
---
+++ dockerInit.sh
@@ -0,0 +1,6 @@
+apt-get -y update
+apt-get -y install sudo
+sudo apt -y install unzip
+sudo apt -y install git
+sudo apt -y install gcc
+sudo apt -y install build-essential gdb
